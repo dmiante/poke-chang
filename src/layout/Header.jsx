b13db10706ac67d73c1dcc8337d719be3a00b88f
damian <the.dmian@live.cl>
2023-06-16 02:06:52 -0400

    ADD routes to icon

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -1,12 +1,14 @@
+import { Link } from 'react-router-dom'
+
 export function Header () {
   return (
     <header className='p-6 border-b shadow-sm bg-amber-400 backdrop-blur lg:px-8'>
       <nav className='flex items-center justify-between mx-auto max-w-7xl' aria-label='Global'>
         <div className='flex place-items-center lg:flex-1'>
-          <a href='/' className='-m-1.5 p-1.5'>
+          <Link to='/' className='-m-1.5 p-1.5'>
             <span className='sr-only'>Pokechang</span>
             <img className='w-auto h-14' src='../src/assets/pokeball.svg' alt='pokeball' />
-          </a>
+          </Link>
           <h2 className='ml-3 text-3xl font-semibold font-flexo'>POKECHANG</h2>
         </div>
         <div className='hidden lg:flex lg:flex-1 lg:justify-end'>
